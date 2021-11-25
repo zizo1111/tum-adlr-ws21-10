4ba1e997a860b2c0b14f4e897f90bc043c9c67da
ozayed <omar.zayed@tum.de>
2021-11-25 18:13:11 +0100

    fixed issues added animation wrap and auto process

--- simulation_env.py
+++ simulation_env.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
-
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
 
 class SimulationEnv():
-    def __init__(self, size, num_discs, num_beacons):
+    def __init__(self, size, num_discs, num_beacons, wrap=False, auto=False, animate=False):
         """
         Initializes the simulation enviroment
         Parameters
@@ -20,32 +21,50 @@ class SimulationEnv():
         self.num_beacons_ = num_beacons
 
         # parameters of the process model
-        self.spring_force_ = 0.05
+        self.spring_force_ = 0.5
         self.drag_force_ = 0.0075
+        
+        #auto process steps
+        self.auto = auto
+
+        #wrap the enviroment
+        self.wrap = wrap
 
         self.discs_ = []
         # init discs
         for disc_num in range(self.num_discs_):
             # draw a random position
-            pos = np.random.uniform(-self.env_size_//2, self.env_size_//2, size=(2))
+            pos = np.random.uniform(0, 2*self.env_size_, size=(2))
             # draw a random velocity
-            vel = np.random.normal(loc=0., scale=1., size=(2)) * 3
+            vel = np.random.normal(loc=0., scale=10., size=(2)) * 3
             self.discs_.append(np.array([pos[0], pos[1], vel[0], vel[1]]))
         
         # init beacons
-        self.beacons_ = []
+        beacons_ = []
         for beacon_num in range(num_beacons):
             axis = np.random.choice([0,1])
             if axis == 0:
-                y_pos = (np.random.uniform(-self.env_size_//2, self.env_size_//2, size=(1))).item()
-                x_pos = np.random.choice([-self.env_size_//2, self.env_size_//2])
-                self.beacons_.append(np.array([x_pos, y_pos]))
+                y_pos = (np.random.uniform(0, 2*self.env_size_, size=(1))).item()
+                x_pos = np.random.choice([0, 2*self.env_size_])
+                beacons_.append(np.array([x_pos, y_pos]))
             else:
-                x_pos = (np.random.uniform(-self.env_size_//2, self.env_size_//2, size=(1))).item()
-                y_pos = np.random.choice([-self.env_size_//2, self.env_size_//2])
-                self.beacons_.append(np.array([x_pos, y_pos]))
+                x_pos = (np.random.uniform(0, 2*self.env_size_, size=(1))).item()
+                y_pos = np.random.choice([0, 2*self.env_size_])
+                beacons_.append(np.array([x_pos, y_pos]))
+        self.beacons_ = np.array(beacons_)
+        
+        # initialize animation
+        if(animate):
+            self.fig = plt.figure()
+
+            self.ani = animation.FuncAnimation(self.fig, self.animate, frames=600,
+                                interval=100, blit=True, init_func=self.init_anim)
+            plt.show()  
     
-    def _process_model(self, state):
+    def get_discs(self):
+        return np.array(self.discs_)
+    
+    def _process_model(self, state, dt):
         """
         Calculates the next state of the disc.
         Parameters
@@ -58,21 +77,32 @@ class SimulationEnv():
             The next state (position and velocity) of the disc
         """
         new_state = np.copy(state)
-        pull_force = - self.spring_force_ * state[:2]
-        drag_force = - self.drag_force_ * state[2:]**2 * np.sign(state[2:])
-        new_state[0] += state[2]
-        new_state[1] += state[3]
-        new_state[2] += pull_force[0] + drag_force[0]
-        new_state[3] += pull_force[1] + drag_force[1]
+        # pull_force = - self.spring_force_ * state[:2]
+        # drag_force = - self.drag_force_ * state[2:]**2 * np.sign(state[2:])
 
+        # new_state = np.copy(state)
+        new_state[0] += dt*state[2]
+        new_state[1] += dt*state[3]
+        # new_state[2] += pull_force[0] + drag_force[0]
+        # new_state[3] += pull_force[1] + drag_force[1]
+        if self.wrap:
+            if new_state[0] > 2*self.env_size_:
+                new_state[0] = new_state[0] - 2*self.env_size_
+            elif new_state[0] < 0:
+                new_state[0] = 2*self.env_size_ - new_state[0]
+            
+            if new_state[1] > 2*self.env_size_:
+                new_state[1] = new_state[1] - 2*self.env_size_
+            elif new_state[1] < 0:
+                new_state[1] = 2*self.env_size_ - new_state[1]
         return new_state
     
-    def update_step(self):
+    def update_step(self, dt):
         """
         Calculates the next state of the discs.
         """
         for disc_num in range(self.num_discs_):
-            self.discs_[disc_num] = self._process_model(self.discs_[disc_num])
+            self.discs_[disc_num] = self._process_model(self.discs_[disc_num], dt)
     
     def get_reading(self, beacon_num):
         """
@@ -84,7 +114,7 @@ class SimulationEnv():
         Returns
         -------
         reading : np.array
-            The current state (position and velocity) of the discs wrt to
+            The current state (position) of the discs wrt to
             given beacon
         """
         reading = []
@@ -92,18 +122,71 @@ class SimulationEnv():
             reading.append(self.discs_[disc_num][:2] - self.beacons_[beacon_num])
         return np.asarray(reading)
     
+    def get_distance(self, beacon_num):
+        """
+        Calculates the absolute distance between the discs and the beacon.
+        Parameters
+        ----------
+        beacon_num : int
+            beacon index
+        Returns
+        -------
+        reading : np.array
+            The he absolute distance between the discs and the
+            given beacon
+        """
+        dists = []
+        for disc_num in range(self.num_discs_):
+            pos = self.discs_[disc_num][:2] - self.beacons_[beacon_num]
+            dists.append(math.sqrt(pos[1]**2 + pos[2]**2))
+        return np.asarray(dists)
+    
     def get_beacons_pos(self):
         """
         returns the beacons postions in the simulation enviroment
         """
         return self.beacons_
-    
-# if __name__ == "__main__":
-#     test_env = SimulationEnv(200, 1, 2)
-#     # print(test_env.discs)
-#     print(test_env.discs_[0][0])
-#     print(test_env.get_reading(0))
-#     test_env.update_step()
-#     # print(test_env.discs)
-#     # print(test_env.discs[0][0])
-#     print(test_env.get_reading(0))
+
+    def init_anim(self):
+        """initialize animation"""
+        # set up figure and animation
+        
+        self.fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        fig_size = self.env_size_ + 100
+        self.ax = self.fig.add_subplot(5, 1, (1,4), aspect='equal', autoscale_on=True,
+                            xlim=(0, 2*self.env_size_), ylim=(0, 2*self.env_size_))
+
+        # particles holds the locations of the particles
+        self.particles, = self.ax.plot([], [], 'bo', ms=6)
+
+        # beacons drawn as red dots
+        self.beacon_particles, = self.ax.plot([], [], 'ro', ms=6)
+        self.beacon_particles.set_data(self.beacons_[:,0], self.beacons_[:,1])
+
+        # rect is the box edge
+        self.rect = plt.Rectangle([0, 0],
+                            2*self.env_size_,
+                            2*self.env_size_,
+                            ec='none', lw=2, fc='none')
+
+        self.ax.add_patch(self.rect)
+        self.particles.set_data([], [])
+        self.rect.set_edgecolor('none')
+
+        return self.particles, self.rect
+
+    def animate(self, i):
+        """perform animation step"""
+        if self.auto:
+            # update step
+            self.update_step(1/5)
+        
+        # update particles the animation
+        self.rect.set_edgecolor('k')
+        self.particles.set_data(self.get_discs()[:,0], self.get_discs()[:,1])
+
+        return self.particles, self.rect
+
+
+if __name__ == "__main__":
+    box = SimulationEnv(200, 1, 2, wrap=True, auto=True, animate=True)
