b2ed5b20b319284ed0e15532c506ec101e7c1658
ozayed <omar.zayed@tum.de>
2021-11-26 03:37:10 +0100

    added collsions

--- simulation_env.py
+++ simulation_env.py
@@ -6,7 +6,7 @@ import math
 
 class SimulationEnv:
     def __init__(
-        self, size, num_discs, num_beacons, wrap=False, auto=False, animate=False
+        self, size, num_discs, num_beacons, mode="def", auto=False, animate=False
     ):
         """
         Initializes the simulation environment
@@ -31,28 +31,29 @@ class SimulationEnv:
         self.auto = auto
 
         # wrap the environment
-        self.wrap = wrap
+        self.mode = mode
 
-        self.discs_ = []
+        discs_ = []
         # init discs
         for disc_num in range(self.num_discs_):
             # draw a random position
-            pos = np.random.uniform(0, 2 * self.env_size_, size=(2))
+            pos = np.random.uniform(0, self.env_size_, size=(2))
             # draw a random velocity
             vel = np.random.normal(loc=0.0, scale=10.0, size=(2)) * 3
-            self.discs_.append(np.array([pos[0], pos[1], vel[0], vel[1]]))
+            discs_.append(np.array([pos[0], pos[1], vel[0], vel[1]]))
+        self.discs_ = np.array(discs_)
 
         # init beacons
         beacons_ = []
         for beacon_num in range(num_beacons):
             axis = np.random.choice([0, 1])
             if axis == 0:
-                y_pos = (np.random.uniform(0, 2 * self.env_size_, size=(1))).item()
-                x_pos = np.random.choice([0, 2 * self.env_size_])
+                y_pos = (np.random.uniform(0, self.env_size_, size=(1))).item()
+                x_pos = np.random.choice([0, self.env_size_])
                 beacons_.append(np.array([x_pos, y_pos]))
             else:
-                x_pos = (np.random.uniform(0, 2 * self.env_size_, size=(1))).item()
-                y_pos = np.random.choice([0, 2 * self.env_size_])
+                x_pos = (np.random.uniform(0, self.env_size_, size=(1))).item()
+                y_pos = np.random.choice([0, self.env_size_])
                 beacons_.append(np.array([x_pos, y_pos]))
         self.beacons_ = np.array(beacons_)
 
@@ -73,7 +74,7 @@ class SimulationEnv:
     def get_discs(self):
         return np.array(self.discs_)
 
-    def _process_model(self, state, dt):
+    def _process_model(self, states, dt):
         """
         Calculates the next state of the disc.
         Parameters
@@ -85,33 +86,36 @@ class SimulationEnv:
         new_state : np.array
             The next state (position and velocity) of the disc
         """
-        new_state = np.copy(state)
-        # pull_force = - self.spring_force_ * state[:2]
-        # drag_force = - self.drag_force_ * state[2:]**2 * np.sign(state[2:])
+        new_state = np.copy(states)
+        new_state[:, 0] += dt * states[:, 2]
+        new_state[:, 1] += dt * states[:, 3]
 
-        # new_state = np.copy(state)
-        new_state[0] += dt * state[2]
-        new_state[1] += dt * state[3]
-        # new_state[2] += pull_force[0] + drag_force[0]
-        # new_state[3] += pull_force[1] + drag_force[1]
-        if self.wrap:
-            if new_state[0] > 2 * self.env_size_:
-                new_state[0] = new_state[0] - 2 * self.env_size_
-            elif new_state[0] < 0:
-                new_state[0] = 2 * self.env_size_ - new_state[0]
+        out_buttom = new_state[:, 0] < 0
+        out_top = new_state[:, 0] > self.env_size_
+        out_left = new_state[:, 1] < 0
+        out_right = new_state[:, 1] > self.env_size_
 
-            if new_state[1] > 2 * self.env_size_:
-                new_state[1] = new_state[1] - 2 * self.env_size_
-            elif new_state[1] < 0:
-                new_state[1] = 2 * self.env_size_ - new_state[1]
+        if self.mode == "collide":
+            new_state[out_buttom, 0] = 0
+            new_state[out_top, 0] = self.env_size_
+            new_state[out_left, 1] = 0
+            new_state[out_right, 1] = self.env_size_
+            new_state[out_top | out_buttom, 2] *= -1
+            new_state[out_left | out_right, 3] *= -1
+
+        elif self.mode == "wrap":
+            new_state[out_top, 0] = new_state[:, 0] - self.env_size_
+            new_state[out_buttom, 0] = self.env_size_ - new_state[:, 0]
+            new_state[out_left, 1] = self.env_size_ - new_state[:, 1]
+            new_state[out_right, 1] = new_state[:, 1] - self.env_size_
+
         return new_state
 
     def update_step(self, dt):
         """
         Calculates the next state of the discs.
         """
-        for disc_num in range(self.num_discs_):
-            self.discs_[disc_num] = self._process_model(self.discs_[disc_num], dt)
+        self.discs_ = self._process_model(self.discs_, dt)
 
     def get_reading(self, beacon_num):
         """
@@ -168,8 +172,8 @@ class SimulationEnv:
             (1, 4),
             aspect="equal",
             autoscale_on=True,
-            xlim=(0, 2 * self.env_size_),
-            ylim=(0, 2 * self.env_size_),
+            xlim=(0, self.env_size_),
+            ylim=(0, self.env_size_),
         )
 
         # discs_vis holds the locations of the discs_vis
@@ -181,7 +185,7 @@ class SimulationEnv:
 
         # rect is the box edge
         self.rect = plt.Rectangle(
-            (0, 0), 2 * self.env_size_, 2 * self.env_size_, ec="none", lw=2, fc="none"
+            (0, 0), self.env_size_, self.env_size_, ec="none", lw=2, fc="none"
         )
 
         self.ax.add_patch(self.rect)
@@ -204,4 +208,4 @@ class SimulationEnv:
 
 
 if __name__ == "__main__":
-    box = SimulationEnv(200, 1, 2, wrap=True, auto=True, animate=True)
+    box = SimulationEnv(200, 1, 2, mode="collide", auto=True, animate=True)
