Use RMSE instead of MSE, and fix error computation

simulation/simulation_env.py
@@ -44,7 +44,7 @@ class SimulationEnv:
 
             # Error propagation
             self.timesteps = 0
-            self.mse = 0
+            self.rmse = 0
 
         # auto process steps
         self.auto_ = auto
@@ -159,12 +159,12 @@ class SimulationEnv:
 
     def _compute_error(self):
         """
-        Computes the error (MSE) at each timestep
-        Note: The error is not averaged over all timesteps!
+        Computes the sum of squared Euclidean errors for all previous timesteps
+        Note: The error is not averaged
 
-        :return: MSE at each timestep
+        :return: sum of squared errors
         """
-        self.mse += np.linalg.norm(self.discs_[0] - self.estimate_)  # 0th disc is always predicted
+        self.rmse += np.linalg.norm(self.discs_[0] - self.estimate_[0])  # 0th disc is always predicted
         self.timesteps += 1
 
     def get_reading(self, beacon_num):
@@ -221,11 +221,14 @@ class SimulationEnv:
 
     def get_error(self):
         """
-        Returns overall error (MSE)
+        Returns overall error (RMSE)
 
-        :return: current MSE, averaged over all timesteps
+        :return: current RMSE, averaged over all timesteps
         """
-        return self.mse / self.timesteps
+        return np.sqrt(self.rmse / self.timesteps)
+
+    def get_timestep(self):
+        return self.timesteps
 
     def get_setup(self):
         """