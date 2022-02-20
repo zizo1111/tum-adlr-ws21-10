WIP: Prepare NLL loss to use the precision matrix from the motion model Note: This is not exactly 100% correct! NLL loss should use some covariance matrices, but I am not sure, if they must come from the GMM at initialization, or from the reparametrization trick in the motion model (and thus, could be learned?)

pf/filters/diff_particle_filter.py
@@ -87,7 +87,7 @@ class DiffParticleFilter(nn.Module):
         soft_resample_alpha = self.hparams["soft_resample_alpha"]
 
         # Apply motion model to predict next particle states
-        self.particle_states = self.motion_model.forward(self.particle_states)
+        self.particle_states, precision_matrix = self.motion_model.forward(self.particle_states)
         assert self.particle_states.shape == (N, M, state_dim)
 
         # Apply observation model to get the likelihood for each particle
@@ -124,7 +124,7 @@ class DiffParticleFilter(nn.Module):
 
         assert self.estimates.shape == (N, state_dim)
 
-        return self.estimates, self.weights, self.particle_states
+        return self.estimates, self.weights, self.particle_states, precision_matrix
 
     def _soft_resample(self, alpha):
         """

pf/models/motion_model.py
@@ -214,16 +214,17 @@ class MotionModel(nn.Module):
         )
 
         # Sample (with gradient) from the resulting distribution -> "reparameterization trick"
-        predicted_particle_states = D.MultivariateNormal(
+        reparam = D.MultivariateNormal(
             loc=predicted_mean, scale_tril=predicted_scale_tril,
-        ).rsample()
+        )
+        predicted_particle_states = reparam.rsample()
 
         # predicted_particle_states[:, :, 0:2] = predicted_particle_states[:, :, 0:2] * 100  # TODO: also a possibility
 
         torch.clamp_(predicted_particle_states[:, :, :2], min=0, max=self.env_size)
         # torch.clamp_(predicted_particle_states[:, :, 3:], min=-5, max=5)
 
-        return predicted_particle_states
+        return predicted_particle_states, reparam.precision_matrix
 
 
 def cholesky_check(value):

pf/simulation/simulation_env.py
@@ -192,7 +192,7 @@ class SimulationEnv:
             self._compute_error()
 
         elif self.dpf_:
-            estimate, weights, particles = self.dpf_(
+            estimate, weights, particles, _ = self.dpf_(
                 torch.from_numpy(np.array([self.get_distance(-1)]).astype(np.float32)),
                 torch.from_numpy(np.array(self.beacons_).astype(np.float32)),
             )

pf/utils/loss.py
@@ -32,7 +32,7 @@ def RMSE(gt, estimate):
 
 # TODO nll
 # https://github.com/akloss/differentiable_filters/blob/821889dec411927658c6ef7dd01c9028d2f28efd/differentiable_filters/contexts/base_context.py#L341
-def NLL(gt, weights, particle_states, covariance=None, reduce_mean=True):
+def NLL(gt, weights, particle_states, covariance=None, precision_matrix=None, reduce_mean=True):
     N = particle_states.shape[0]  # batch size
     M = particle_states.shape[1]  # number of particles
     state_dim = particle_states.shape[-1]
@@ -47,6 +47,8 @@ def NLL(gt, weights, particle_states, covariance=None, reduce_mean=True):
     weights /= torch.sum(weights, dim=-1, keepdims=True)
     assert weights.shape == (N, M)
 
+    if precision_matrix is not None:  # prefer precision matrix over covariance
+        precision_matrix = precision_matrix.view([1, N, M, state_dim, state_dim])
     covariance = torch.diag_embed(covariance)
 
     if diff.ndim > 3:
@@ -58,14 +60,24 @@ def NLL(gt, weights, particle_states, covariance=None, reduce_mean=True):
         diff = torch.reshape(diff, [N, M, state_dim, 1])
         covariance = torch.tile(covariance[None, None, :, :], [N, 1, 1, 1])
 
-    exponent = torch.matmul(
-        torch.matmul(torch.transpose(diff, -2, -1), torch.inverse(covariance)), diff
-    )
+    if precision_matrix is not None:  # prefer precision matrix over covariance
+        exponent = torch.matmul(
+            torch.matmul(torch.transpose(diff, -2, -1), precision_matrix), diff
+        )
+    else:
+        exponent = torch.matmul(
+            torch.matmul(torch.transpose(diff, -2, -1), torch.inverse(covariance)), diff
+        )
     exponent = torch.reshape(exponent, [N, sl, M])
 
-    normalizer = torch.log(torch.det(covariance)) + (
-        state_dim * torch.log(2 * torch.tensor(math.pi))
-    )
+    if precision_matrix is not None:  # prefer precision matrix over covariance
+        normalizer = torch.log(torch.det(torch.inverse(precision_matrix))) + (
+            state_dim * torch.log(2 * torch.tensor(math.pi))
+        )
+    else:
+        normalizer = torch.log(torch.det(covariance)) + (
+            state_dim * torch.log(2 * torch.tensor(math.pi))
+        )
     normalizer = torch.reshape(normalizer, [N, sl, M])
 
     log_like = -0.5 * (exponent + normalizer)