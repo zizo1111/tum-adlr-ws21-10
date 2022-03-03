fixed variance tolerance

pf/filters/diff_particle_filter.py
@@ -59,7 +59,8 @@ class DiffParticleFilter(nn.Module):
                 torch.hstack(
                     ((torch.rand(M, 2) * 2) - 1, torch.randn(M, state_dim - 2))
                 ),
-                torch.rand(M, state_dim) + torch.full((M, state_dim), 1e-5),
+                torch.rand(M, state_dim)
+                + torch.full((M, state_dim), 1e-9, dtype=torch.float32),
             ),
             1,
         )